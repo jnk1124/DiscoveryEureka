f07621317269b4577a61beecd613a97f9aa1d655
jnk1124 <jnk1124@naver.com>
2021-08-17 23:37:03 +0900

    Initial commit

---
+++ src/test/java/com/jnk1124/discovery/DiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnk1124.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
